6b7f0a54cf058bf20b61c151d69e5406c11e5135
Schalk W. Cronjé <yw-github@af.org.za>
2017-06-10 22:08:51 +0200

    Delay classloading of GradleRunner until it is actually required (#74)

--- src/main/groovy/org/ysb33r/gradle/gradlerunner/internal/GradleRunnerFactory.groovy
+++ src/main/groovy/org/ysb33r/gradle/gradlerunner/internal/GradleRunnerFactory.groovy
@@ -15,6 +15,7 @@ package org.ysb33r.gradle.gradlerunner.internal
 
 import groovy.transform.CompileStatic
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.ysb33r.gradle.gradlerunner.GradleRunnerPlugin
 
 import java.lang.reflect.Method
@@ -27,17 +28,18 @@ import java.lang.reflect.Method
 class GradleRunnerFactory {
 
     GradleRunnerFactory(Project project) {
-        URL[] urls = project.configurations.getByName(GradleRunnerPlugin.CLASSPATH_CONFIGURATION).files.collect { File f ->
-            f.absoluteFile.toURI().toURL()
-        } as URL[]
-        URLClassLoader loader = URLClassLoader.newInstance(urls,this.getClass().classLoader)
-        this.gradleRunner = loader.loadClass('org.gradle.testkit.runner.GradleRunner')
+        configuration = project.configurations.getByName(GradleRunnerPlugin.CLASSPATH_CONFIGURATION)
     }
 
     def createRunner() {
+        URL[] urls = configuration.files.collect { File f ->
+            f.absoluteFile.toURI().toURL()
+        } as URL[]
+        URLClassLoader loader = URLClassLoader.newInstance(urls,this.getClass().classLoader)
+        Class gradleRunner = loader.loadClass('org.gradle.testkit.runner.GradleRunner')
         Method createMethod = gradleRunner.getDeclaredMethod('create')
         createMethod.invoke(gradleRunner)
     }
 
-    private Class gradleRunner
+    private Configuration configuration
 }
