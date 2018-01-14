b8e61b91e53e7fdf6691677c8ca1bd7567345dab
Schalk W. Cronjé <ysb33r@gmail.com>
2018-01-14 15:08:11 +0000

    WIP: Testing incoming PR

--- src/test/groovy/org/ysb33r/gradle/gradletest/TestGeneratorSpec.groovy
+++ src/test/groovy/org/ysb33r/gradle/gradletest/TestGeneratorSpec.groovy
@@ -13,14 +13,7 @@
  */
 package org.ysb33r.gradle.gradletest
 
-import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.file.SourceDirectorySet
-import org.gradle.api.tasks.SourceSet
-import org.gradle.api.tasks.TaskState
 import org.ysb33r.gradle.gradletest.internal.GradleTestSpecification
-import spock.lang.Specification
-
 
 class TestGeneratorSpec extends GradleTestSpecification {
 
@@ -34,6 +27,9 @@ class TestGeneratorSpec extends GradleTestSpecification {
             testDir.mkdirs()
             if(it != 'delta') {
                 new File(testDir,'build.gradle').text = ''
+            }
+            if(it == 'beta') {
+                new File(testDir,'testTwo.gradle.kts').text = ''
             }
             if (it=='eta') {
                 new File(testDir,'build.gradle.kts').text = ''
@@ -59,31 +55,41 @@ class TestGeneratorSpec extends GradleTestSpecification {
         String initScriptContent = new File(genTask.outputDir.parentFile.parentFile,'init.gradle').text
 
         then: "The test names reflect the directories under gradleTest"
-        testNames.containsAll(['alphaGroovyDSL','betaGroovyDSL','gammaGroovyDSL','etaGroovyDSL','etaKotlinDSL'])
+        testNames.containsAll(['alpha', 'beta', 'gamma', 'eta'])
+        assert genTask.testMap['alpha'].kotlinBuildFiles.size() == 0
+        assert genTask.testMap['alpha'].groovyBuildFiles.size() == 1
+        assert genTask.testMap['beta'].kotlinBuildFiles.size() == 1
+        assert genTask.testMap['beta'].groovyBuildFiles.size() == 1
+        assert genTask.testMap['gamma'].kotlinBuildFiles.size() == 0
+        assert genTask.testMap['gamma'].groovyBuildFiles.size() == 1
+        assert genTask.testMap['eta'].kotlinBuildFiles.size() == 1
+        assert genTask.testMap['eta'].groovyBuildFiles.size() == 1
 
         and: "Groovy+Spock test files are generated in the source set directory under the build directory"
         genTask.outputDir.exists()
-        new File(genTask.outputDir,"AlphaGroovyDSLCompatibilitySpec.groovy").exists()
-        new File(genTask.outputDir,"BetaGroovyDSLCompatibilitySpec.groovy").exists()
-        new File(genTask.outputDir,"GammaGroovyDSLCompatibilitySpec.groovy").exists()
-        new File(genTask.outputDir,"EtaKotlinDSLCompatibilitySpec.groovy").exists()
-        new File(genTask.outputDir,"EtaGroovyDSLCompatibilitySpec.groovy").exists()
+        new File(genTask.outputDir,"Alpha_BuildGroovyDSLCompatibilitySpec.groovy").exists()
+        new File(genTask.outputDir,"Beta_BuildGroovyDSLCompatibilitySpec.groovy").exists()
+        new File(genTask.outputDir,"Beta_TestTwoKotlinDSLCompatibilitySpec.groovy").exists()
+        new File(genTask.outputDir,"Gamma_BuildGroovyDSLCompatibilitySpec.groovy").exists()
+        new File(genTask.outputDir,"Eta_BuildGroovyDSLCompatibilitySpec.groovy").exists()
+        new File(genTask.outputDir,"Eta_BuildKotlinDSLCompatibilitySpec.groovy").exists()
+//        new File(genTask.outputDir,"EtaGroovyDSLCompatibilitySpec.groovy").exists()
 
         and: "Subdirectories without a build.gradle file will not be included"
         !testNames.containsAll(['delta'])
-        !new File(genTask.outputDir,"DeltaCompatibilitySpec.groovy").exists()
+        !new File(genTask.outputDir,"DeltabuildCompatibilitySpec.groovy").exists()
 
         and: "An initscript is created"
         initScriptContent.contains("classpath fileTree ('${new File(buildDir,'libs').toURI()}'.toURI())")
         initScriptContent.contains("dirs '${new File(buildDir,'gradleTest/repo').toURI()}'.toURI()")
 
         when: "The generated source file is inspected"
-        String source = new File(genTask.outputDir,"AlphaGroovyDSLCompatibilitySpec.groovy").text
+        String source = new File(genTask.outputDir,"Alpha_BuildGroovyDSLCompatibilitySpec.groovy").text
 
         then:
         source.contains "package ${genTask.testPackageName}"
         source.contains "result.task(':runGradleTest')" 
-        source.contains "def \"AlphaGroovyDSL : #version\"()"
+        source.contains "def \"Alpha_BuildGroovyDSL : #version\"()"
         source.contains "version << ['3.999','3.998','4.997']"
     }
 
